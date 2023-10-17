e594edf29c108f0187fbdcd95df117398bb07b1c
lyy <2424809934@qq.com>
2023-10-17 12:31:32 +0800

    Add license statement

--- opendb-core/src/main/java/net/openio/opendb/db/SynchronousWriteBatch.java
+++ opendb-core/src/main/java/net/openio/opendb/db/SynchronousWriteBatch.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the OpenIO.Net under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.openio.opendb.db;
 
 import net.openio.opendb.log.Log;
@@ -54,7 +70,7 @@ public class SynchronousWriteBatch extends WriteBatch {
   @Override
   void step(List<Log> list) {
     try {
-      if(!list.isEmpty()) {
+      if (!list.isEmpty()) {
         walStorage.addLogs(list);
       }
     } catch (IOException e) {
@@ -64,7 +80,7 @@ public class SynchronousWriteBatch extends WriteBatch {
 
     try {
       this.notifyAll();
-    }catch (Exception e){
+    } catch (Exception e) {
 
     }
 
@@ -80,7 +96,7 @@ public class SynchronousWriteBatch extends WriteBatch {
         this.wait();
       } catch (Exception e) {
       }
-    }while (this.writeSequenceNumber<snapshot.sequenceNumber.getTimes());
+    } while (this.writeSequenceNumber < snapshot.sequenceNumber.getTimes());
     snapshot.pre.next = null;
   }
 
